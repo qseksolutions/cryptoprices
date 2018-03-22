ab7dc2c3a37e4696977b896378e2b6946f3047fd
Jaydip Patel <jaydip@qseksolutions.com>
2018-03-22 10:12:49 +0530

    no message

--- src/app/protfolio/protfolio.component.ts
+++ src/app/protfolio/protfolio.component.ts
@@ -1,15 +1,112 @@
 import { Component, OnInit } from '@angular/core';
+import { CoinService } from '../coin.service';
+import * as myGlobals from './../global';
+import { defer } from 'q';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import { ToasterContainerComponent, ToasterService, ToasterConfig } from 'angular2-toaster';
+import { Title, Meta } from '@angular/platform-browser';
+
+declare var $: any;
 
 @Component({
   selector: 'app-protfolio',
   templateUrl: './protfolio.component.html',
-  styleUrls: ['./protfolio.component.css']
+  styleUrls: ['./protfolio.component.css'],
+  providers: [CoinService],
 })
 export class ProtfolioComponent implements OnInit {
 
-  constructor() { }
+  private toasterService: ToasterService;
+  
+  public base_url: any = myGlobals.base_url;
+  public loginData: any = myGlobals.login_ses;
+  public base_sing: any = myGlobals.base_sing;
+  allcoin: any;
+  allcurrency: any;
+  portfoliolist: any;
+  profitlosslist: any;
+  public model: any;
+  public modelcur: any;
+  public modeldate: any;
+  totalcost: any = 0;
+  value: any = 0;
+  overolsum: any = 0;
+  overolper: any = 0;
+
+  constructor(private coinservice: CoinService, toasterService: ToasterService, private title: Title, private meta: Meta) {
+    localStorage.setItem('sorton', null);
+    localStorage.setItem('sortby', null);
+    this.toasterService = toasterService;
+    if (this.loginData == null) {
+      window.location.href = this.base_url;
+    }
+  }
 
   ngOnInit() {
+    /* const curl = window.location.href;
+    this.coinservice.gettestseometa(curl).subscribe(resData => {
+      if (resData.status === true) {
+        this.title.setTitle(resData.data.title);
+        this.meta.addTag({ name: 'description', content: resData.data.description });
+        this.meta.addTag({ name: 'keywords', content: resData.data.keywords });
+        this.meta.addTag({ name: 'author', content: 'coinlisting' });
+        this.meta.addTag({ name: 'robots', content: resData.data.robots });
+        this.meta.addTag({ name: 'title', content: ' www.coinlisting.io' });
+      }
+    }); */
+    this.coinservice.profitlosslist().subscribe(resData => {
+      if (resData.status === true) {
+        for (let i = 0; i < resData.data.length; i++) {
+          this.totalcost += resData.data[i]['totalcost'];
+          this.value += resData.data[i]['current_price'] * resData.data[i]['coin_amount'];
+        }
+        this.overolsum = this.value;
+        this.overolper = (this.value - this.totalcost) / this.totalcost * 100;
+        this.profitlosslist = resData.data;
+      } else {
+        this.profitlosslist = '';
+      }
+      console.log(this.profitlosslist);
+    });
+    this.coinservice.portfoliolist().subscribe(resData => {
+      if (resData.status === true) {
+        this.portfoliolist = resData.data;
+      } else {
+        this.portfoliolist = '';
+      }
+      console.log(this.portfoliolist);
+    });
+    this.coinservice.getallcoin('').subscribe(resData => {
+      if (resData.status === true) {
+        this.allcoin = resData.data;
+      }
+    });
+    this.coinservice.getmaincurrencylist('').subscribe(resData => {
+      if (resData.status === true) {
+        this.allcurrency = resData.data;
+      }
+    });
   }
+
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .map(term => term === '' ? []
+        : this.allcoin.filter(v => v.id.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+
+  formatter = (x: { name: string, symbol: string }) => x.name + ' (' + x.symbol + ')';
+
+  searchcur = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .map(termcur => termcur === '' ? []
+        : this.allcurrency.filter(v => v.currency_symbol.toLowerCase().indexOf(termcur.toLowerCase()) > -1).slice(0, 10))
+
+  formattercur = (x: { currency_symbol: string }) => x.currency_symbol;
+  formattersign = (x: { currency_sign: string }) => x.currency_sign;
+
+
 
 }

c51f30c6f518a9ddc1cd1feb4d06288950c62ed9
amit kansagara <amit@qseksolutions.com>
2018-03-23 16:38:53 +0530

    no message

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,6 +1,6 @@
 export const header = '';
-//export const base_url = 'http://localhost:4200/';
-export const base_url = 'http://stagging.cryptoprices.com/';
+export const base_url = 'http://localhost:4200/';
+// export const base_url = 'http://stagging.cryptoprices.com/';
 // export const api_url = 'https://api.coinlisting.io/v1/';
 export const api_url = 'http://74.124.217.247:3000/v1/';
 
